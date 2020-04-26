8bf905084dfe5832f663f95d0b8b62f7d420fe64
zxf <3080001352@qq.com>
2020-04-26 09:54:19 +0800

    zxf第1提交

---
+++ ppees-dao/src/main/java/com/csdj/mapper/zxf/MedicalCertificateMapper.java
@@ -0,0 +1,9 @@
+package com.csdj.mapper.zxf;
+
+
+import com.csdj.pojo.Record;
+
+public interface MedicalCertificateMapper {
+
+    Record getrecordByid(Integer rid);
+}
---
+++ ppees-service/src/main/java/com/csdj/service/zxf/MedicalCertificateService.java
@@ -0,0 +1,7 @@
+package com.csdj.service.zxf;
+
+import com.csdj.pojo.Record;
+
+public interface MedicalCertificateService {
+    Record getrecordByid(Integer rid);
+}
