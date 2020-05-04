b61c7ca02292affbe2ecfc3558cd0801f30382ec
Alaskalaofei <3070649228@qq.com>
2020-05-04 13:16:54 +0800

    lx添加短信模块实体类

---
+++ ppees-common/src/main/java/com/csdj/pojo/smstemplate.java
@@ -0,0 +1,22 @@
+package com.csdj.pojo;
+
+public class smstemplate {
+    private int smsid;
+    private String smstemplatecontent;
+
+    public int getSmsid() {
+        return smsid;
+    }
+
+    public void setSmsid(int smsid) {
+        this.smsid = smsid;
+    }
+
+    public String getSmstemplatecontent() {
+        return smstemplatecontent;
+    }
+
+    public void setSmstemplatecontent(String smstemplatecontent) {
+        this.smstemplatecontent = smstemplatecontent;
+    }
+}
